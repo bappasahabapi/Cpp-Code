97e8385571908f3101a527187cad99770531d6b8
Bappa Saha <69105298+bappasaha@users.noreply.github.com>
2020-08-10 01:38:59 +0600

    এখানে দাদার রাজ্য + এর সীমানা নিয়ে ব্যখ্যা আছে

---
+++ 02.Area program/prob 07.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float length;
+    float area;
+    cout<<"plese enter the length of the squre ";
+    cin>>length;
+    area =length *length;
+    cout<<"area of the squre is :"<<area<<"sq.units";
+
+    return 0;
+}
+
+
